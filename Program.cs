8ee34d5ee3997a5b3d0d07e72f3affc7b52261e2
Mohamed Khaled Elzalook <114666874+Morales020@users.noreply.github.com>
2022-12-20 17:57:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Project_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter The Start Number : ");
+            int num1 = int .Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter The End Number : ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("The Perfect Numbers Between {0} And {1} is : ", num1, num2);
+            for (int i =num1;i<=num2;i++) { 
+            int sum = 0;
+            for (int j =1;j<i;j++) {
+                    if (i % j == 0)
+                    {
+                        sum +=j;
+                    }
+                    }
+                if (sum == i&& sum !=0)
+                {
+                    Console.WriteLine(sum);
+                }
+            }
+        }
+    }
+}
